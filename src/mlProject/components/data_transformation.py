c6ea71ff4ae4fde5c3fc7078e0c3b44a9e8bab46
Rutvik46 <rutvikroy46@gmail.com>
2025-07-21 20:25:13 -0400

    data transformation pipeline added

---
+++ src/mlProject/components/data_transformation.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from mlProject import logger
+from mlProject.entity.config_entity import DataTransformationConfig
+import os
+
+class DataTransformation:
+    def __init__(self, config: DataTransformationConfig):
+        self.config = config
+        
+
+    # All data transformation techniques such as normalization, encoding, Scalar, PCA, etc. can be implemented here.
+    
+    def data_preprocessing(self) -> pd.DataFrame:
+
+        logger.info("Starting data preprocessing...")
+
+        # Load the data
+        data = pd.read_csv(self.config.data_path)
+        data.dropna(inplace=True)
+        data.drop_duplicates(inplace=True)
+
+        # Map categorical columns to numeric
+        data['Sex'] = data['Sex'].map({'M': 1, 'F': 0})
+        data['ExerciseAngina'] = data['ExerciseAngina'].map({'Y': 1, 'N': 0})
+
+
+        data['ChestPainType'] = data['ChestPainType'].map({
+            'ATA': 0,
+            'NAP': 1,
+            'ASY': 2,
+            'TA': 3
+        })
+
+        data['RestingECG'] = data['RestingECG'].map({
+            'Normal': 0,
+            'ST': 1,
+            'LVH': 2
+        })
+
+        data['ST_Slope'] = data['ST_Slope'].map({
+            'Up': 0,
+            'Flat': 1,
+            'Down': 2
+        })
+
+        return data
+
+    def train_test_split(self, data: pd.DataFrame) -> None:
+        
+        # Splitting the data into train and test sets
+        # 75% of the data will be used for training and 25% for testing
+        train, test = train_test_split(data, test_size=0.25, random_state=42)
+
+        train.to_csv(os.path.join(self.config.root_dir ,'train.csv'), index=False)
+        test.to_csv(os.path.join(self.config.root_dir ,'test.csv'), index=False)
+
+        logger.info(f"Train and test data saved at {self.config.root_dir}")
+        logger.info(f"Train data shape: {train.shape}")
+        logger.info(f"Test data shape: {test.shape}")
---
+++ src/mlProject/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,27 @@
+from mlProject.components.data_transformation import DataTransformation
+from mlProject.config.configuration import ConfigurationManager
+from mlProject import logger
+
+STAGE_NAME = "Data Transformation Stage"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+            config_manager = ConfigurationManager()
+            data_transformation_config = config_manager.get_data_transformation_config()    
+            data_transformation = DataTransformation(config=data_transformation_config)
+            processed_data = data_transformation.data_preprocessing()
+            data_transformation.train_test_split(processed_data)
+            
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
